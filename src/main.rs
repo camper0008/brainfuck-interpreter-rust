b59af6dc46462a0bd81cd03ce1650a7f0be998e5
camper0008 <spetakel123@gmail.com>
2021-12-22 11:05:44 +0100

    fix issues, implement ',' operator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 mod context;
 
+use std::io::stdin;
+use std::io::Read;
+
+fn get_char() -> u8 {
+    let mut buffer: [u8; 1] = [0];
+    let mut take = stdin().take(1);
+    (take.read(&mut buffer)).expect("unable to read from buffer");
+    buffer[0]
+}
+
 fn brainfuck(code: String, mut ctx: context::Context) {
+    if ctx.stack.len() == 0 {
+        ctx.stack.push(0);
+    }
+
     let mut cursor: u32 = 0;
     while cursor < code.len().try_into().unwrap() {
         let c = code
@@ -23,29 +37,29 @@ fn brainfuck(code: String, mut ctx: context::Context) {
                 }
             }
             '.' => {
-                //putChar(memory[memoryIndex]);
+                print!("{}", ctx.stack[ctx.stack_index] as char);
             }
             ',' => {
-                //char input = getChar();
-                //memory[memoryIndex] = input;
+                ctx.stack[ctx.stack_index] = get_char();
             }
             '>' => {
-                if ctx.stack_index + 1 > ctx.stack.len() {
+                if ctx.stack_index + 2 > ctx.stack.len() {
                     ctx.stack.push(0)
                 }
+                ctx.stack_index += 1;
             }
             '<' => {
                 if ctx.stack_index == 0 {
-                    ctx.stack_index = ctx.stack.len() - 1
+                    panic!("pointer went below 0");
                 } else {
-                    ctx.stack_index -= 1
+                    ctx.stack_index -= 1;
                 }
             }
             '[' => {
                 if ctx.stack[ctx.stack_index] == 0 {
                     let pair = ctx
                         .get_bracket_pair(cursor)
-                        .expect("could not find bracket pair");
+                        .expect("could not find bracket pair end");
                     cursor = pair.end;
                 }
             }
@@ -53,17 +67,18 @@ fn brainfuck(code: String, mut ctx: context::Context) {
                 if ctx.stack[ctx.stack_index] != 0 {
                     let pair = ctx
                         .get_bracket_pair(cursor)
-                        .expect("could not find bracket pair");
+                        .expect("could not find bracket pair start");
                     cursor = pair.start;
                 }
             }
             _ => {}
         }
+        cursor += 1;
     }
 }
 
 fn main() {
-    let code: String = ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.".to_string();
+    let code: String = ",.".to_string();
     let mut ctx = context::Context {
         bracket_pairs: Vec::new(),
         stack: Vec::new(),
